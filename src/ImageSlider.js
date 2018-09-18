b0391553dd3a5b7ce4a8ba490f582de1c6f46301
Anusha Konatala <anu.konatala@gmail.com>
2018-09-18 06:37:35 +0000

    automatically backed up by learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,19 @@
-// your ImageSlider code here!
+import React, { Component } from 'react';
+import CatComponent from './CatComponent.js'
+import { FordQuoteComponent } from './FordQuoteComponent'
+import MouseComponent from '../MouseComponent'
+
+class App extends Component {
+  render() {
+    // your code in the return statement below!
+    return (
+      <div className="App">
+        <CatComponent />
+        <FordQuoteComponent />
+        <MouseComponent />
+      </div>
+    );
+  }
+}
+
+export default ImageSlider;
